ef97da22ca989e42da93580d49e33494ec43423c
Justis Gipson <justis710@gmail.com>
2019-04-05 14:36:50 -0400

    updated dependencies

--- src/Components/FooterComponent/BottomNav.js
+++ src/Components/FooterComponent/BottomNav.js
@@ -5,48 +5,45 @@ import {Form, Input, Button, /*FormText,*/ Modal, ModalBody, ModalHeader, ModalF
 
 class Footer extends Component{
   constructor(props){
-      super(props);
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-          isOpen: false
-      };
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
   }
 
   toggle(){
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
-    toggleModal = () => {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
   render(){
       return(
-          
           <div className='fixed-bottom'>
-              <h3 >Created by <a  href="https://justisgipson.com" target="blank">justisGipson</a> -- 2019  <Button className="btn" size="lg" onClick={this.toggle}></Button></h3>
-              <div>
+            <h3 >Created by <a  href="https://twitter.com/j__gipson" target="blank">justisGipson</a> -- 2019  <Button className="btn" size="lg" onClick={this.toggle}></Button></h3>
+            <div>
               <Modal isOpen={this.state.isOpen} toggle={this.toggle} centered={true}>
-                  <ModalHeader>Contact for bugs you find, issues you have, or just to say it's rad.</ModalHeader>
-                  <Form action="https://formspree.io/justis710@gmail.com"
-                          method="POST" target="blank">
-                      <ModalBody>
-                          
-                              <Input type="email" name="email" placeholder="Email:" required/>
-                              <br />
-                              <Input type="textarea" name="message" placeholder="Enter your message:" />
-                          
-                      </ModalBody>
-                  <ModalFooter>
+                <ModalHeader>Contact for bugs you find, issues you have, or just to say it's rad.</ModalHeader>
+                <Form action="https://formspree.io/justis710@gmail.com"
+                  method="POST" target="blank">
+                    <ModalBody>
+                      <Input type="email" name="email" placeholder="Email:" required/>
+                      <br />
+                      <Input type="textarea" name="message" placeholder="Enter your message:" /> 
+                    </ModalBody>
+                    <ModalFooter>
                       <Button className="btn btn-secondary" size="sm" type="submit" value="send">Submit</Button>
                       <Button className='btn btn-secondary' size='sm' onClick={this.toggle}>Cancel</Button>
-                  </ModalFooter>
+                    </ModalFooter>
                   </Form>
-              </Modal>
+                </Modal>
               </div>
           </div>
       )
