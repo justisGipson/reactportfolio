316a25003b3140af71c06b32bd7d514d2127fe9e
Justis Gipson <justis710@gmail.com>
2019-05-24 11:57:39 -0400

    fixed dependency issues, started building componenets

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Radium from 'radium';
+import Header from './Components/NavComponent/Header'
 import './App.css';
+import BottomNav from './Components/FooterComponent/BottomNav';
+
+class App extends React.Component{
+
+  render() {
+    return (
+      <Router>
+        <div>
+        <Header />
+        <BottomNav />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default Radium(App);
--- src/Components/FooterComponent/BottomNav.js
+++ src/Components/FooterComponent/BottomNav.js
@@ -30,13 +30,13 @@ class Footer extends Component{
             <h3 >Created by <a  href="https://twitter.com/j__gipson" target="blank">justisGipson</a> -- 2019  <Button className="btn" size="lg" onClick={this.toggle}></Button></h3>
             <div>
               <Modal isOpen={this.state.isOpen} toggle={this.toggle} centered={true}>
-                <ModalHeader>Contact for bugs you find, issues you have, or just to say it's rad.</ModalHeader>
+                <ModalHeader>Feel free to contact me!</ModalHeader>
                 <Form action="https://formspree.io/justis710@gmail.com"
                   method="POST" target="blank">
                     <ModalBody>
                       <Input type="email" name="email" placeholder="Email:" required/>
                       <br />
-                      <Input type="textarea" name="message" placeholder="Enter your message:" /> 
+                      <Input type="textarea" name="message" placeholder="Enter your message:" />
                     </ModalBody>
                     <ModalFooter>
                       <Button className="btn btn-secondary" size="sm" type="submit" value="send">Submit</Button>
